e6d9e745c3f429a2a36fcb7d5b1143d5939468eb
Ong Jing Hanz <ojing-ha@u90z08s04.42kl.edu.my>
2023-02-17 13:38:49 +0800

    Made own usleep function

--- srcs/philo_routine.c
+++ srcs/philo_routine.c
@@ -6,11 +6,21 @@
 /*   By: ojing-ha <ojing-ha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/26 00:17:49 by ojing-ha          #+#    #+#             */
-/*   Updated: 2023/02/17 00:18:03 by ojing-ha         ###   ########.fr       */
+/*   Updated: 2023/02/17 13:32:39 by ojing-ha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
+
+void	ft_sleep(int sleeptime, int eat_start, struct timeval start_time)
+{
+	while (1)
+	{
+		usleep(10);
+		if ((get_time(start_time) - eat_start) >= sleeptime)
+			return ;
+	}
+}
 
 void	choose_fork(t_philo *philo, t_fork **left, t_fork **right)
 {
@@ -33,7 +43,8 @@ void	eat(t_philo	*philo)
 	philo->total_ate++;
 	philo->last_ate = get_time(philo->main->start_time);
 	pthread_mutex_unlock(&philo->philo_mutex);
-	usleep(philo->input->eat_time * 1000);
+	ft_sleep(philo->input->eat_time,
+		get_time(philo->main->start_time), philo->main->start_time);
 	pthread_mutex_unlock(&philo->left->fork_mutex);
 	pthread_mutex_unlock(&philo->right->fork_mutex);
 }
@@ -41,13 +52,16 @@ void	eat(t_philo	*philo)
 void	*routine(void	*philo)
 {
 	if ((((t_philo *)philo)->n % 2) == 0)
-		usleep(2500);
+		ft_sleep(25, get_time(((t_philo *)philo)->main->start_time),
+			((t_philo *)philo)->main->start_time);
 	eat((t_philo *)philo);
 	while (1)
 	{
 		get_message((t_philo *)philo,
 			((t_philo *)philo)->n, "is sleeping", YELLOW);
-		usleep(((t_philo *)philo)->input->sleep_time * 1000);
+		ft_sleep(((t_philo *)philo)->input->eat_time,
+			get_time(((t_philo *)philo)->main->start_time),
+			((t_philo *)philo)->main->start_time);
 		get_message((t_philo *)philo,
 			((t_philo *)philo)->n, "is thinking", BLUE);
 		eat((t_philo *)philo);
